0d8222b1b2da185bbd11961d8701e58a875ca308
select766 <select766@outlook.jp>
2022-02-13 13:21:56 +0900

    Initial Commit

---
+++ NeneShogiSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeneShogiSwift
+//
+//  Created by Masatoshi Hidaka on 2022/02/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NeneShogiSwift/NeneShogiSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeneShogiSwiftApp.swift
+//  NeneShogiSwift
+//
+//  Created by Masatoshi Hidaka on 2022/02/13.
+//
+
+import SwiftUI
+
+@main
+struct NeneShogiSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
