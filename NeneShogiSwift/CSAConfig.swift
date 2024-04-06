4af83d20efa553ad6f21993b2db5d04451eb5550
Masatoshi Hidaka <hidaka@edgeintelligence.jp>
2024-04-06 18:19:18 +0900

    持ち時間をCSAから受信したものを使用

--- NeneShogiSwift/CSAConfig.swift
+++ NeneShogiSwift/CSAConfig.swift
@@ -6,8 +6,4 @@ struct CSAConfig: Codable {
     var loginName: String
     var loginPassword: String
     var ponder: Bool
-    // 開始時の持ち時間
-    var timeTotalSec: Double
-    // 手番が来た時の加算時間（フィッシャー）※秒読み未実装
-    var timeIncrementSec: Double
 }
