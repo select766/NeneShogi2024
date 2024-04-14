afe7443acf4fdcb7b46b832153bda882967376ac
select766 <select766@outlook.jp>
2024-04-14 16:56:21 +0900

    PVを2行表示

--- NeneShogiSwift/PVView.swift
+++ NeneShogiSwift/PVView.swift
@@ -5,6 +5,15 @@ struct PVView: View {
     let gridSize = CGFloat(32)
     
     var searchProgress: SearchProgress?
+    
+    var pvString: String {
+        if let searchProgress = searchProgress {
+            // reservesSpace: true テキストが短くても2行分のスペースを確保する
+            return searchProgress.pv.map({dMove in dMove.toPrintString()}).joined(separator: "")
+        } else {
+            return "読み筋"
+        }
+    }
 
     var body: some View {
         VStack(alignment: .leading) {
@@ -13,14 +22,8 @@ struct PVView: View {
                 Text("NPS: \(searchProgress?.nps?.description ?? "-")").foregroundStyle(.black)
                 Spacer()
             }
-            HStack {
-                if let searchProgress = searchProgress {
-                    Text(searchProgress.pv.map({dMove in dMove.toPrintString()}).joined(separator: " ")).foregroundStyle(.black)
-                } else {
-                    Text("読み筋").foregroundStyle(.black)
-                }
-                Spacer()
-            }
+            // reservesSpace: true テキストが短くても2行分のスペースを確保する
+            Text(pvString).lineLimit(2, reservesSpace: true).font(Font(UIFont.monospacedDigitSystemFont(ofSize: gridSize * 0.5, weight: .medium))).foregroundStyle(.black).frame(alignment: .leading)
         }.frame(maxWidth: .infinity).padding(.all, 1).background(Color.white.edgesIgnoringSafeArea([])).border(Color.black, width: 1)
     }
 }
