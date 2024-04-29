d397a02e4a4e379eec40d04ee9062b4bdc6c884c
select766 <select766@outlook.jp>
2024-04-29 12:47:18 +0900

    本番想定パラメータ

--- NeneShogiSwift/DefaultUSIOptions.swift
+++ NeneShogiSwift/DefaultUSIOptions.swift
@@ -10,11 +10,14 @@ import Foundation
 let defaultUSIOptions = """
 setoption DNN_Model1 value
 setoption DNN_Batch_Size1 value 8
+setoption UCT_Threads1 value 4
 setoption USI_Ponder value true
+setoption USI_Hash value 1024
 setoption Stochastic_Ponder value true
 setoption BookFile value user_book1.db
 setoption BookDir value .
 setoption BookDepthLimit value 0
 setoption BookMoves value 32
 setoption IgnoreBookPly value true
+setoption name NetworkDelay2 value 2200
 """.split(separator: "\n").map({String($0)})
